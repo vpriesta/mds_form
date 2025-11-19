17740d6c85738a113060471e66c3bed1c995b1ec
vpriesta <165936646+vpriesta@users.noreply.github.com>
2025-11-19 15:46:33 +0700

    Update 1_Form_Page_.py

--- pages/1_Form_Page_.py
+++ pages/1_Form_Page_.py
@@ -155,9 +155,9 @@ else:
 if edit_id and notes:
     st.info(f"ℹ️ Catatan Revisi:\n{notes}\n\nTanggal Pemeriksaan: {datetime.fromisoformat(verif_date).date().strftime("%d %B %Y")}")
 
-if status == "rejected":
+if edit_id and status == "rejected":
     st.warning(f"❌ Alasan Ditolak:\n{rn}\n\nTanggal Ditolak: {datetime.fromisoformat(reject_date).date().strftime("%d %B %Y")}")
-elif status == "verified":
+elif edit_id and status == "verified":
     st.success(f"✅ Metadata telah diverifikasi dan diterima\n\nTanggal Diterima: {datetime.fromisoformat(verified_at).date().strftime("%d %B %Y")}")
 
 # =====================================================
