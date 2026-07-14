ab14b2c9682ad24cc9022a7b8fdf3b9b8749bbd5
vpriesta <165936646+vpriesta@users.noreply.github.com>
2026-07-14 09:19:34 +0700

    Update Dashboard_.py

--- Dashboard_.py
+++ Dashboard_.py
@@ -33,7 +33,7 @@ def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
     st.write(f"DEBUG: Input text hashed is: {hash_password(password_input)}")
     
-st.write(f"DEBUG: Input text hashed is: {hash_password(password_input)}")
+st.write(f"DEBUG: Input text hashed is: {hash_password(password)}")
 # --- Retrieve credentials from secrets.toml ---
 users = st.secrets["users"]
 roles = st.secrets["roles"]
